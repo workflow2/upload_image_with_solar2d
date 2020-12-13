7cf8b6af927ea907dd36ce54d568a13e3c61295f
workflow2 <70626031+workflow2@users.noreply.github.com>
2020-12-13 19:20:34 +0100

    Add files via upload

---
+++ Libs/utils.lua
@@ -0,0 +1,7 @@
+local utils = {}
+
+function utils.ReturnPositionVariable()
+    return display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight
+end
+
+return utils
